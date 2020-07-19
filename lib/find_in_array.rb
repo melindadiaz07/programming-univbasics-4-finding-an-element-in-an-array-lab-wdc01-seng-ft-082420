f19ba763bc92494c6057efaa0278bc08393467bd
Melinda Diaz <melindadiaz@me.com>
2020-07-19 16:05:29 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,14 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ if array.include?(value_to_find)
+   counter = 0 
+   while counter < array.length do
+     if array[counter] == value_to_find
+       return counter
+     else
+       counter +=1
+     end
+     
+     
+   end
+ end
 end
